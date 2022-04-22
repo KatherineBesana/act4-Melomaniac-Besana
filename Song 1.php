f7a396bc1137ca8b284af5e01c183427d245b511
BesanaKatherine <besanakatherine@gmail.com>
2022-04-22 13:29:40 +0300

    initial files

---
+++ Song 1.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="Where your hearts belongs to.." content="" />
+        <meta name=" Katherine Besana" content="" />
+        <title>Playlist dell'umore</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">  Playlist dell'umore </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Playlist dell'umore</h1>
+                <p class="lead">Where your hearts belongs to.. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>I choose</h2>";
+                        echo "<p class='lead '>Allesia Cara </p>";
+                           
+                       echo "<p>  C         F  </p>"; 
+                      echo "<p> All of my life</p>";
+                        echo "<p>  Am          F  </p>"; 
+                      echo "<p> I thought I was right  </p>";
+                        echo "<p>  C         F  </p>"; 
+                    echo "<p> Looking for something new</p>";
+                    echo "<p> Stuck in my ways</p>";
+                    echo "<p>  Am          F  </p>"; 
+                    echo "<p> Like old-fashioned days </p>";
+                     echo "<br/>";
+    
+                     echo "<p> But all the roads led me to you</p>";
+                                                                      
+                      echo "<p>  Dm                         F             </p>";      
+                     echo "<p> The house that you live in don't make it a home</p>";
+                     echo "<p>  C                          G  </p>"; 
+                     echo "<p> But feeling lonely don't mean you're alone</p>";
+                      echo "<p>  Dm                         F             </p>"; 
+                      echo "<p> People in life, they will come and they'll leave</p>";
+                       echo "<p>  C                  Am               G            </p>"; 
+                     echo "<p> But if I had a choice I know where I would be</p>";
+                     echo "<br/>";
+
+                      
+                       echo "<p>            F                           C             </p>"; 
+                     echo "<p> Through the lows and the highs I will stay by your side</p>";
+                       echo "<p>       G                               Am         G            </p>"; 
+                      echo "<p> There's no need for goodbyes, now I'm seeing the light</p>";
+                       echo "<p>       F                                     G             </p>"; 
+                     echo "<p> When the sky turns to grey and there's nothing to say</p>";
+                      echo "<p>        G                   Am             </p>"; 
+                      echo "<p> At the end of the day I choose you</p>";
+                      echo "<br/>";
+
+                     echo "<p>  C             F             </p>"; 
+                    echo "<p> Now I found the strength</p>";
+                     echo "<p>  Am         F             </p>"; 
+                     echo "<p> To make a change</p>";
+                      echo "<p>  C                        F             </p>"; 
+
+                    echo "<p> And look at the magic I found</p>";
+                    echo "<p>  Am             F             </p>"; 
+                      echo "<p> No matter the name</p>";
+                     echo "<p> Or where you came from</p>";
+                     echo "<p>  C                        F             </p>"; 
+                    echo "<p> 'Cause no one has much figured out</p>";
+                    echo "<br/>";
+
+                      echo "<p>  Dm                        F             </p>"; 
+                     echo "<p> The house that you live in don't make it a home</p>";
+                     echo "<p>  C                            G              </p>"; 
+                      echo "<p> But feeling lonely don't mean you're alone</p>";
+                      echo "<p>  Dm                         F             </p>"; 
+                    echo "<p> I finally found where I feel I belong</p>";
+                    echo "<p>      C               Am               G             </p>";   
+                     echo "<p> And I know you'll be there with wide open arms</p>";
+                     echo "<br/>";
+
+
+                        echo "<p>            F                           C             </p>"; 
+                     echo "<p> Through the lows and the highs I will stay by your side</p>";
+                       echo "<p>       G                               Am         G            </p>"; 
+                      echo "<p> There's no need for goodbyes, now I'm seeing the light</p>";
+                       echo "<p>       F                                     G             </p>"; 
+                     echo "<p> When the sky turns to grey and there's nothing to say</p>";
+                      echo "<p>        G                   Am             </p>"; 
+                      echo "<p> At the end of the day I choose you</p>";
+                      echo "<br/>";
+
+
+
+                    echo "<p>   F    CF Am           </p>"; 
+                      echo "<p> I choose you</p>";
+
+                        echo "<p>            F                           C             </p>"; 
+                     echo "<p> Through the lows and the highs I will stay by your side</p>";
+                       echo "<p>       G                               Am         G            </p>"; 
+                      echo "<p> There's no need for goodbyes, now I'm seeing the light</p>";
+                       echo "<p>       F                                     G             </p>"; 
+                     echo "<p> When the sky turns to grey and there's nothing to say</p>";
+                      echo "<p>        G                   Am             </p>"; 
+                      echo "<p> At the end of the day I choose you</p>";
+                      echo "<br/>";
+
+
+
+                       echo "<p>   F    CF Am           </p>"; 
+                      echo "<p> I choose you</p>";
+                      echo "<br/>";
+
+
+
+
+
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Katherine Besana</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
