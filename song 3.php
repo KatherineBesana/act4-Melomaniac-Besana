18722eb9e70b977540d35d6477caa17c7b16d6f7
BesanaKatherine <besanakatherine@gmail.com>
2022-04-22 13:47:05 +0300

    initial files

---
+++ song 3.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="Where your hearts belongs to.." content="" />
+        <meta name="Katherine Besana" content="" />
+        <title>Playlist dell'umore</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">  Playlist dell'umore </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Playlist dell'umore</h1>
+                <p class="lead">Where your hearts belongs to.. </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'> Easy on Me</h2>";
+                        echo "<p class='lead '> Adele </p>";
+                          echo "<p>               C               Am7       </p>";
+                           echo "<p> There ain't no gold in this river</p>";
+                            echo "<p>                 Em                Fmaj7       </p>";
+                           echo "<p> That I've been washing my hands in forever</p>";
+                            echo "<p> G            C                 Am7       </p>";
+                           echo "<p> I know there is hope in these waters</p>";
+                            echo "<p>               Em                     </p>";
+                           echo "<p> But I can't bring myself to swim</p>";
+                            echo "<p>       Fmaj7                  F            </p>";
+                           echo "<p> When I am drowning in this silence</p>";
+                                              Fmaj7
+                           echo "<p> Baby, let me in</p>";
+                           echo "<br/>";
+
+                           echo "<p> G C Am7     G         </p>";
+                           echo "<p> Go easy on me, baby</p>";
+                            echo "<p>  Em    Dm7      </p>";
+                           echo "<p> I was still a child</p>";
+                            echo "<p>  C     Fmaj7         G   </p>";
+                           echo "<p> Didn't get the chance to</p>";
+                            echo "<p>  Fm                    </p>";
+                           echo "<p> Feel the world around me</p>";
+                            echo "<p>  Em      Dm7             C  </p>";
+                           echo "<p> I had no time to choose what I chose to do</p>";
+                            echo "<p>       C  Am7 Em Fmasj7      </p>";
+                           echo "<p> So go easy on me</p>";
+                           echo "<br/>";
+
+                           echo "<p>  G            C                      Am7  </p>";
+                           echo "<p> There ain't no room for things to change</p>";
+                           echo "<p>              Em                         Fmaj7 </p>";
+                           echo "<p> When we are both so deeply stuck in our ways</p>";
+                           echo "<p>  G          C           </p>";
+                           echo "<p> You can't deny how hard I have tried</p>";
+                           echo "<p>  Am7         Em                   Fmaj7 </p>";
+                           echo "<p> I changed who I was to put you both first</p>";
+                           echo "<p>                Fmaj7    </p>";
+                           echo "<p> But now I give up</p>";
+                           echo "<br/>";
+
+
+                          echo "<p> G C Am7     G         </p>";
+                           echo "<p> Go easy on me, baby</p>";
+                            echo "<p>  Em    Dm7      </p>";
+                           echo "<p> I was still a child</p>";
+                            echo "<p>  C     Fmaj7         G   </p>";
+                           echo "<p> Didn't get the chance to</p>";
+                            echo "<p>  Fm                    </p>";
+                           echo "<p> Feel the world around me</p>";
+                            echo "<p>  Em      Dm7             C  </p>";
+                           echo "<p> I had no time to choose what I chose to do</p>";
+                            echo "<p>       C  Am7 Em Fmasj7      </p>";
+                           echo "<p> So go easy on me</p>";
+                           echo "<br/>";
+
+
+
+
+                          echo "<p>  C             Am7          </p>";
+                           echo "<p> I had good intentions</p>";
+                           echo "<p>          Em         </p>";
+                           echo "<p> And the highest hopes</p>";
+                           echo "<p>                   Fmaj7          </p>";
+                           echo "<p> But I know right now</p>";
+                           echo "<p>                           Fmaj7         </p>";
+                           echo "<p> It probably doesn't even show</p>";
+                           echo "<br/>";
+
+                              
+
+
+                          echo "<p> G C Am7     G         </p>";
+                           echo "<p> Go easy on me, baby</p>";
+                            echo "<p>  Em    Dm7      </p>";
+                           echo "<p> I was still a child</p>";
+                            echo "<p>  C     Fmaj7         G   </p>";
+                           echo "<p> Didn't get the chance to</p>";
+                            echo "<p>  Fm                    </p>";
+                           echo "<p> Feel the world around me</p>";
+                            echo "<p>  Em      Dm7             C  </p>";
+                           echo "<p> I had no time to choose what I chose to do</p>";
+                            echo "<p>       C  Am7 Em Fmasj7      </p>";
+                           echo "<p> So go easy on me</p>";
+                           echo "<br/>";
+
+
+
+
+                        
+                         
+                           
+
+                        
+                    
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Katherine Besana</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
